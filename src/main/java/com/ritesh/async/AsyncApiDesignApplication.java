89ca5b75422198f35bb4345f1d6461529aae1902
Ritesh Tiwari <ritesh.tiwari@idrive.com>
2023-10-16 18:03:28 +0530

    initial commit

---
+++ src/main/java/com/ritesh/async/AsyncApiDesignApplication.java
@@ -0,0 +1,13 @@
+package com.ritesh.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncApiDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncApiDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ritesh/async/AsyncApiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApiDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
